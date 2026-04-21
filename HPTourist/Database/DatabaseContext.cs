0575443731d66d21d25f3fd0cc000d66ada6cba2
Abigail <contact@abiwillow.com>
2026-04-21 19:21:43 +0200

    Update DatabaseContext.cs

--- HPTourist/Database/DatabaseContext.cs
+++ HPTourist/Database/DatabaseContext.cs
@@ -13,6 +13,7 @@ public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbCont
    public DbSet<Language> Languages => Set<Language>();
    public DbSet<Identification> Identificatios => Set<Identification>();
    public DbSet<EHIC> EHICs => Set<EHIC>();
+   public DbSet<Allergy> Allergies => Set<Allergy>();
 
    protected override void OnModelCreating(ModelBuilder modelBuilder)
    {
