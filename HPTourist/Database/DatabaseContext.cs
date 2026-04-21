de187db9663ddb237d80664f9ab14eb9bf57e59d
Jelle <Jelle_koomen@outlook.com>
2026-04-21 11:36:14 +0200

    Added migrations:)

--- HPTourist/Database/DatabaseContext.cs
+++ HPTourist/Database/DatabaseContext.cs
@@ -1,7 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using HPTourist.Models;
 
 namespace HPTourist.Database;
 
 public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 {
+    public DbSet<PrescriptionRequest> PrescriptionRequests { get; set; } = default!;
+
+    public DbSet<Prescription> Prescriptions { get; set; } = default!;
 }
