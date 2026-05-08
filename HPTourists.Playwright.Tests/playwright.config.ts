b308434c6552c36d71061548640a5621f3513b45
Nukterinos <lobkehabets@gmail.com>
2026-05-08 13:27:22 +0200

    adjust ports

--- HPTourists.Playwright.Tests/playwright.config.ts
+++ HPTourists.Playwright.Tests/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-     baseURL: process.env.BASE_URL || 'http://localhost:5432',
+     baseURL: process.env.BASE_URL || 'http://localhost:5148',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -68,7 +68,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
    webServer: {
      command: 'dotnet run --project ../HPTourist/HPTourist.csproj',
-    url: 'http://localhost:5432',
+    url: 'http://localhost:5148',
     reuseExistingServer: !process.env.CI,
    },
 });
