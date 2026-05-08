d1a07ae568fbaa91e2e257627682d459f8e1613e
Nukterinos <lobkehabets@gmail.com>
2026-05-08 13:21:01 +0200

    adjust config +yaml

--- HPTourists.Playwright.Tests/playwright.config.ts
+++ HPTourists.Playwright.Tests/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-     baseURL: process.env.BASE_URL || 'http://localhost:5148',
+     baseURL: process.env.BASE_URL || 'http://localhost:5432',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -63,31 +63,12 @@ export default defineConfig({
       dependencies: ['setup'],
     },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
    webServer: {
      command: 'dotnet run --project ../HPTourist/HPTourist.csproj',
     url: 'http://localhost:5432',
- //    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI,
    },
 });
