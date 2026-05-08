91dbd609e91703a9ffa0f85e00601c952137ef31
Nukterinos <lobkehabets@gmail.com>
2026-05-08 11:00:33 +0200

    extend test for adding medication

--- HPTourists.Playwright.Tests/Pages/medication-page.ts
+++ HPTourists.Playwright.Tests/Pages/medication-page.ts
@@ -1,19 +1,19 @@
 import { type Page, type Locator } from '@playwright/test';
- 
+
 export class MedicationPage {
   readonly page: Page;
-    readonly addMedication: Locator;
+  readonly addMedication: Locator;
 
- 
+
   constructor(page: Page) {
     this.page = page;
     this.addMedication = this.page.getByRole('link', { name: 'Medicatie toevoegen' });
 
- 
+
   }
- 
+
   async goto() {
     await this.page.goto('/patient/medication');
   }
- 
+
 }
--- HPTourists.Playwright.Tests/tests/medication.spec.ts
+++ HPTourists.Playwright.Tests/tests/medication.spec.ts
@@ -4,11 +4,30 @@ import { MedicationPage} from '../Pages/medication-page';
 test.beforeEach(async ({ page }) => {
   const medication = new MedicationPage(page);
   await medication.goto();
-  await page.getByRole('link', { name: 'English' }).click();
+  await page.getByRole('link', { name: 'Nederlands' }).click();
 });
 
-test('add new medication', async ({ page }) => {
+test('open add new medication page', async ({ page }) => {
   const medication = new MedicationPage(page);
     await medication.addMedication.click()
+    await expect(page.getByRole('heading', { name: 'Medicatie toevoegen' })).toBeVisible();
+});
+
+test('Add new medication', async ({ page }) => {
+  const medication = new MedicationPage(page);
+    await medication.addMedication.click()
+    await expect(page.getByRole('heading', { name: 'Medicatie toevoegen' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Medicatienaam' }).fill('Paracetamol');
+    await page.getByRole('textbox', { name: 'Werkzame stof' }).fill('Paracetamol');
+    await page.getByRole('textbox', { name: 'ATC-code (optioneel)' }).fill('N02BE01');
+    await page.getByLabel('Farmaceutische vorm').selectOption('Tablet');
+    await page.getByRole('button', { name: 'Opslaan' }).click();
+
+    const table = page.locator('table.table');
+    const row = table.locator('tbody tr').filter({ hasText: 'Paracetamol' });
+    await expect(row).toBeVisible();
+    await expect(row.locator('td').filter({ has: page.locator('span.badge') })).toContainText('In behandeling');
+    await row.getByRole('button', { name: 'Verwijderen' }).click();
+
 
 });
